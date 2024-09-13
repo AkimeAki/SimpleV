0f46c5d1e5d7aced254de5bd76d9473dd174e4de
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-09-14 05:25:27 +0900

    nodeバージョン指定

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,27 +7,19 @@ const site = "https://simple-v.aki.wtf";
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        port: 14501,
-        host: "0.0.0.0"
-    },
-    site,
-    trailingSlash: "never",
-    integrations: [sitemap({
-        filter: (page) => {
-            if (page === `${site}/view`) {
-                return false;
-            }
-
-            return true;
-        }
-		}), react()],
-    build: {
-        format: "file"
-    },
-    vite: {
-        define: {
-            global: "window"
-        }
-    }
+	server: {
+		port: 14501,
+		host: "0.0.0.0"
+	},
+	site,
+	trailingSlash: "never",
+	integrations: [sitemap(), react()],
+	build: {
+		format: "file"
+	},
+	vite: {
+		define: {
+			global: "window"
+		}
+	}
 });
